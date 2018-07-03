cb92d74931d133d7e000f9594072a5d3de9fa093
xiahongyun <xiahongyun@jd.com>
2018-07-03 18:54:19 +0800

    dubbo

---
+++ dubbo-demo/dubbo-demo-provider/src/main/java/org/apache/dubbo/provider/Provider.java
@@ -0,0 +1,13 @@
+package org.apache.dubbo.provider;
+
+public class Provider {
+    public static void main(String[] args) throws Exception{
+        //Prevent to get IPV6 address,this way only work in debug mode
+        //But you can pass use -Djava.net.preferIPv4Stack=true,then it work well whether in debug mode or not
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-provider.xml"});
+        context.start();
+
+        System.in.read();
+    }
+}
