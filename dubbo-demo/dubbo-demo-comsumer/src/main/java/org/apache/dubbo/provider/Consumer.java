7712ac74a2decadaac4c4883413fcd796228a09c
xhyflowcloud <1872081614@qq.com>
2018-07-03 22:16:48 +0800

    dubbo

---
+++ dubbo-demo/dubbo-demo-comsumer/src/main/java/org/apache/dubbo/provider/Consumer.java
@@ -0,0 +1,23 @@
+package org.apache.dubbo.provider;
+
+import org.apache.dubbo.demo.DemoService;
+
+public class Consumer {
+    public static void main(String[] args) {
+        //Prevent to get IPV6 address,this way only work in debug mode
+        //But you can pass use -Djava.net.preferIPv4Stack=true,then it work well whether in debug mode or not
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
+        context.start();
+        DemoService demoService = (DemoService) context.getBean("demoService");
+        while(true){
+            try {
+                Thread.sleep(1000);
+                String hello = demoService.sayHello("world");
+                System.out.println(hello);
+            }catch (Throwable throwable){
+                throwable.printStackTrace();
+            }
+        }
+    }
+}
